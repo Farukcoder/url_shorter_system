c137b87b0ab8abb106fd52061a700006de8e7ee2
Md Omar Faruk <67540985+Farukcoder@users.noreply.github.com>
2024-10-22 10:33:22 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ShortenedUrlController;
 
 Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
 Route::post('/user/register', [AuthController::class, 'store'])->name('user.register');
 
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
-    // Route::apiResource('')
+     Route::apiResource('url-shorten', ShortenedUrlController::class);
 });
